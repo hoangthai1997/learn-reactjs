c152f177bb7ac19b6c7a45e09d7b53ad7a7502df
hoangthai1997 <vuongquochoangthai@gmail.com>
2023-02-10 19:06:13 +0700

    05-04 Folder structure

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+Loding.propTypes = {};
+
+function Loding(props) {
+  return <div>Loading</div>;
+}
+
+export default Loding;
