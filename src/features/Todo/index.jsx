b38341ab76ba765f315152118eaa3d48828960d0
hoangthai1997 <vuongquochoangthai@gmail.com>
2023-02-10 19:28:13 +0700

    05-05 TodoList

--- src/features/Todo/index.jsx
+++ src/features/Todo/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import TodoList from "./components/TodoList/index.jsx";
 
 TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-  return <div></div>;
+  const todoList = [
+    {
+      id: 1,
+      title: "Eat",
+    },
+    {
+      id: 2,
+      title: "Sleep",
+    },
+    {
+      id: 3,
+      title: "Code",
+    },
+  ];
+  return (
+    <div>
+      <h3>Todo List</h3>
+      <TodoList todoList={todoList} />
+    </div>
+  );
 }
 
 export default TodoFeature;
